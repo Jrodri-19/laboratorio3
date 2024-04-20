a920e8bf2268cba63bd47a799c2045caad961189
Jrodri-19 <145525087+Jrodri-19@users.noreply.github.com>
2024-04-20 13:07:04 -0500

    Add files via upload

---
+++ print_matriz.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+void print_matriz(int **m, int f,int c){
+	int **p= m;
+	int *q=nullptr;
+	while(p<m+f){
+		q=*p;
+		while(q<*p+c){
+			cout<<*q<<"\t";
+			q++;
+		}
+		cout<<"\n";
+		p++;
+	}
+}
+int main() {
+	int matriz[4][3]={{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+	int **arr= new int *[4];
+	for(int i=0;i<4;i++){
+		arr[i]=matriz[i];
+	}
+	print_matriz(arr, 4, 3);
+	return 0;
+}
+
